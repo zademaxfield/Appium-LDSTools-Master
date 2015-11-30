6f82fa86658984b537f94230a5b2c8febfac0cd0
Zade <zade.maxfield@ldschurch.org>
2015-11-30 11:50:38 -0700

    Fixed email reports

--- src/LDSTest/ExecutionListener.java
+++ src/LDSTest/ExecutionListener.java
@@ -1,19 +1,38 @@
 package LDSTest;
 
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.xml.utils.URI;
 import org.testng.IExecutionListener;
 
+
+
 public class ExecutionListener implements IExecutionListener {
+	//File myFile = new File("test-output/emailable-report.html");
+	File myFile = new File("test-output/custom-report.html");
+	
+	//myFile = "test-output/emailable-report.html";
 	
 	@Override
 	public void onExecutionStart() {
 		// TODO Auto-generated method stub
-		System.out.println("Test Started!");
+		//System.out.println("Test Started!");
 	}
 	
 	@Override
     public void onExecutionFinish() {
 		String[] args = null;
-		System.out.println("Notify by mail, TestNG is finished");
+		System.out.println("Test is finished... Sending email");
+		/*
+		try {
+			java.awt.Desktop.getDesktop().open(myFile);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		*/
 		SendFileEmail.main(args);
 	}
 
