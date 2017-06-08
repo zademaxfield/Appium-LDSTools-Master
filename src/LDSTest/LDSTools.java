07ba03bdccaafce1effc9a03fd5ef85b0086b0a6
Zade <zade.maxfield@ldschurch.org>
2017-06-08 10:38:02 -0600

    Remove header tests for now

--- src/LDSTest/LDSTools.java
+++ src/LDSTest/LDSTools.java
@@ -506,8 +506,8 @@ public class LDSTools {
 		
 		//LeaderBishopricDirectory("ngiBPC1", false, os);
 		//LeaderBishopricDrawerOrgMissionary("ngiBPC1", false, os);
-		//LeaderBishopricReport("ngiBPC1", false, os);
-		LeaderBishopricHTVT("ngiBPC1", false, os); 
+		LeaderBishopricReport("ngiBPC1", false, os);
+		//LeaderBishopricHTVT("ngiBPC1", false, os); 
 
 		//LeaderBishopricReport("ngiMC1", true, os); //Assistant Ward Clerk - Membership
 		//LeaderBishopricReport("ngiBPC2", false, os); //Bishopric 2nd Counselor  
