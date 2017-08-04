0564fffb9ba39705917c0b2f16b7c3dbd7052a38
Zade <zade.maxfield@ldschurch.org>
2017-08-04 08:33:31 -0600

    Just a test

--- src/LDSTest/LDSTools.java
+++ src/LDSTest/LDSTools.java
@@ -521,12 +521,12 @@ public class LDSTools {
 		//LeaderNonBishopricHTVT("LDSTools39", "Ward Council", os); //Sunday School Pres
 		//LeaderNonBishopricReport("LDSTools32", "Ward Council", os);
 		
-		//LeaderBishopricDirectory("ngiBPC1", false, os);
+		LeaderBishopricDirectory("ngiBPC1", false, os);
 		//LeaderBishopricDrawerOrgMissionary("ngiBPC1", false, os);
 		//LeaderBishopricReport("ngiBPC1", false, os);
 		//LeaderBishopricHTVT("ngiBPC1", false, os); 
 
-		LeaderBishopricReport("ngiMC1", true, os); //Assistant Ward Clerk - Membership
+		//LeaderBishopricReport("ngiMC1", true, os); //Assistant Ward Clerk - Membership
 		//LeaderBishopricReport("ngiBPC2", false, os); //Bishopric 2nd Counselor  
 		
 		//AssistantWardClerkMembershipReport(os);
