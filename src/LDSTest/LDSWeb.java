c59f77e96aa511b9353e33b83023325aa56acd59
Zade <zade.maxfield@ldschurch.org>
2017-10-16 09:56:17 -0600

    Changes for using intellij

--- src/LDSTest/LDSWeb.java
+++ src/LDSTest/LDSWeb.java
@@ -60,7 +60,7 @@ import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
-import bsh.org.objectweb.asm.Type;
+//import bsh.org.objectweb.asm.Type;
 
 
 public class LDSWeb {
