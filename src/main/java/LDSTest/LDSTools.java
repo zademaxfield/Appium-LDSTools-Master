e151b5e6a9f14e1e02c524e1aba6f6d8b53baf56
Zade <zade.maxfield@ldschurch.org>
2018-02-16 14:39:42 -0700

    minor fixes

--- src/main/java/LDSTest/LDSTools.java
+++ src/main/java/LDSTest/LDSTools.java
@@ -540,7 +540,7 @@ public class LDSTools {
 		//LeaderNonBishopricHTVT("LDSTools39", "Ward Council", os); //Sunday School Pres
 		//LeaderNonBishopricReport("LDSTools32", "Ward Council", os);
 		
-		//LeaderBishopricDirectory("ngiBPC1", false, os);
+		LeaderBishopricDirectory("ngiBPC1", false, os);
 		//LeaderBishopricDrawerOrgMissionary("ngiBPC1", false, os);
 		//LeaderBishopricReport("ngiBPC1", false, os);
 		//LeaderBishopricHTVT("ngiBPC1", false, os);
@@ -548,7 +548,7 @@ public class LDSTools {
 		//LeaderBishopricReport("ngiMC1", true, os); //Assistant Ward Clerk - Membership
 		//LeaderBishopricReport("ngiBPC2", false, os); //Bishopric 2nd Counselor  
 		
-		AssistantWardClerkMembershipReport(os);
+		//AssistantWardClerkMembershipReport(os);
 
 		//SundaySchoolPresidentMissionary(os);
 
